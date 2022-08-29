1a836b330f3c5408f2eec067a923ee773d90adf3
manasd044 <110493824+manasd044@users.noreply.github.com>
2022-08-29 16:16:07 +0530

    Add files via upload

---
+++ addtwono.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a,b;
+    cout<<"enter the no: ";
+    cin>>a;
+    cout<<"enter the no: ";
+    cin>>b;
+    cout<<"addition of two no: "<<a+b;
+    return 0;
+}
